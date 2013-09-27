4f1e07a6cf6a1d4e16726ecdb2a60370590b77f3
peinguin <pingvein@gmail.com>
2013-09-27 16:43:24 +0300

    conference full view

--- client/www/js/app/config.js
+++ client/www/js/app/config.js
@@ -1,4 +1,4 @@
 define({
 	authHeader: 'api_key',
-	baseUrl : window.location.host == "local.phonegap"?'http://localhost:8002/':'http://myconference.php.poltava.ua/'
+	baseUrl : window.location.host == "local.phonegap"?'http://localhost:8002/':'http://myconfernce.php.poltava.ua/'
 });
