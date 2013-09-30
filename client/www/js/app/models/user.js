a007281d9f2b034edb6c5db6c5970be552d54f9a
peinguin <pingvein@gmail.com>
2013-09-30 19:34:11 +0300

    social auth

--- client/www/js/app/models/user.js
+++ client/www/js/app/models/user.js
@@ -168,7 +168,7 @@ define(
 
 			},
 			twitter: function(){
-				
+				window.open(cfg.baseUrl + 'twitter', 'twittet Auth', "height=200,width=200");
 			},
 			linkedin: function(){
 				(function(d){
