11b1a5918268aa04ee77651e90b6681ffd17e78d
peinguin <pingvein@gmail.com>
2013-10-01 12:14:13 +0300

    facebook login

--- client/www/js/app/models/user.js
+++ client/www/js/app/models/user.js
@@ -172,8 +172,7 @@ define(
 					gapi.auth.authorize(
 						{
 							client_id:'774864135362.apps.googleusercontent.com',
-							scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/plus.me',
-							access_type: 'offline'
+							scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/plus.me'
 						},
 						function(a){
 							$.post(
