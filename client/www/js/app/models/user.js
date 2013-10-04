41722b1689765a35255191db795a3cb5a3560191
peinguin <pingvein@gmail.com>
2013-10-04 20:04:37 +0300

    facebook auth fix

--- client/www/js/app/models/user.js
+++ client/www/js/app/models/user.js
@@ -167,7 +167,7 @@ define(
 				    }
 
 					FB.getLoginStatus(function(response) {
-						if(response.status == "not_authorized" || response.status == "uknown"){
+						if(response.status == "not_authorized" || response.status == "unknown"){
 						    FB.login(function(response, a) {
 							    if (response.authResponse) {
 							    	sendAccessToken(response);
